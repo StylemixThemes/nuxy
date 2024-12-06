85058218c3c9f165e029ff1ea4d6c4764dc7b0aa
Vlad Zakiev <zakiev.vladislav@stylemix.net>
2024-12-06 09:33:06 +0500

    Group Title Control Tooltip added

--- metaboxes/general_components/es6/group_title.js
+++ metaboxes/general_components/es6/group_title.js
@@ -19,25 +19,68 @@ Vue.component('wpcfto_group_title', {
 				window.open(this.field_button.link, '_blank')
 			}
 		},
+		handleTooltipHover: function () {
+			var tooltip = document.querySelector(
+				'.wpcfto-group-title-button-tooltip-content'
+			)
+			var settings = document.querySelector('.stm_metaboxes_grid')
+			if (tooltip && settings) {
+				settings.classList.add('stm_metaboxes_grid--tooltip-hover')
+				tooltip.classList.add('show-tooltip')
+			}
+		},
+		handleTooltipLeave: function () {
+			var tooltip = document.querySelector(
+				'.wpcfto-group-title-button-tooltip-content'
+			)
+			var settings = document.querySelector('.stm_metaboxes_grid')
+			if (tooltip && settings) {
+				settings.classList.remove('stm_metaboxes_grid--tooltip-hover')
+				tooltip.classList.remove('show-tooltip')
+			}
+		},
+		handleScroll: function () {
+			this.handleTooltipLeave()
+		},
+	},
+	mounted: function () {
+		window.addEventListener('scroll', this.handleScroll)
+	},
+	beforeDestroy: function () {
+		window.removeEventListener('scroll', this.handleScroll)
 	},
 	template: `
-        <div class="wpcfto_generic_field wpcfto_generic_field__group_title" :class="field_preview_position">
-            <i :class="field_icon"></i>
-            <div class="wpcfto-group-title-wrapper">
-            <wpcfto_fields_aside_before :fields="fields" :field_label="field_label"></wpcfto_fields_aside_before>
-                <div v-if="field_button" class="wpcfto-group-title-button-container">
-                    <a 
-                        :href="field_button.link" 
-                        target="_blank" 
-                        class="wpcfto-group-title-button" 
-                        @click.prevent="handleButtonClick" 
-                        :title="field_button.tooltip"
-                        style="position: relative;"
-                    >
-                        <i :class="field_button.icon"></i> {{ field_button.text }}
-                    </a>
-                </div>
-            </div>
+    <div 
+      class="wpcfto_generic_field wpcfto_generic_field__group_title" 
+      :class="field_preview_position"
+    >
+      <i :class="field_icon"></i>
+      <div class="wpcfto-group-title-wrapper">
+        <wpcfto_fields_aside_before :fields="fields" :field_label="field_label"></wpcfto_fields_aside_before>
+        <div 
+          v-if="field_button" 
+          class="wpcfto-group-title-button-container"  
+        >
+          <a 
+            :href="field_button.link" 
+            target="_blank" 
+            class="wpcfto-group-title-button" 
+            :class="{'tooltip-exists': field_button.tooltip && field_button.tooltip.trim() !== ''}" 
+            @click.prevent="handleButtonClick" 
+            :title="field_button.tooltip"
+            style="position: relative;"
+            @mouseover="handleTooltipHover"
+            @mouseleave="handleTooltipLeave"
+          >
+            <i :class="field_button.icon"></i> {{ field_button.text }}
+            <span
+              v-if="field_button.tooltip" 
+              class="wpcfto-group-title-button-tooltip-content" 
+              v-html="field_button.tooltip"
+            ></span>
+          </a>
         </div>
-    `,
+      </div>
+    </div>
+  `,
 })
