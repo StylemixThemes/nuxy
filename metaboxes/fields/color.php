1151a86c0755de29ad663a1725d3370d1cdf9fe1
Rashid Sharipov <rashid.sharipov@stylemix.net>
2025-02-24 02:38:17 +0500

    Set default value for color field on delete

--- metaboxes/fields/color.php
+++ metaboxes/fields/color.php
@@ -21,5 +21,6 @@ $field = "data['{$section_name}']['fields']['{$field_name}']";
 				:field_name="'<?php echo esc_attr($field_name); ?>'"
 				:field_id="'<?php echo esc_attr($field_id); ?>'"
 				:field_value="<?php echo esc_attr($field_value); ?>"
+				:default_value="<?php echo esc_attr($default_value); ?>"
 				@wpcfto-get-value="<?php echo esc_attr($field_value); ?> = $event">
 </wpcfto_color>
